f08e1bd7abb40dac8bbe99e23160215ed983c0e8
JG-Jeong <jo128g@naver.com>
2025-07-21 20:08:17 +0900

    data preprocessing

---
+++ .ipynb_checkpoints/kaggle_titanic-checkpoint.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# 데이터 로드
+train_df = pd.read_csv('train.csv')
+test_df = pd.read_csv('test.csv')
+
+print(train_df.isnull().sum())
--- kaggle_titanic.py
+++ kaggle_titanic.py
@@ -3,11 +3,35 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# 데이터 로드
+# data load
 train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
 
-# 데이터 미리보기
-print(train_df.head())
-print(train_df.info())
-print(train_df.describe())
+# data check - finding missing values(결측치)
+print(train_df.isnull().sum())
+
+# visualize missing values (heatmap)
+sns.heatmap(train_df.isnull(), cbar=False, cmap='viridis')
+plt.show() 
+
+# Data search
+train_df.head()
+
+# data preprocessing - filling missing values as mean
+# 결측치(없는데이터)를 평균값으로 채움
+train_df['Age'].fillna(train_df['Age'].mean(), inplace=True)
+test_df['Age'].fillna(test_df['Age'].mean(), inplace=True)
+
+# data preprocessing - filling missing values as mode
+# 결측치(없는데이터)를 최빈값으로 채움
+train_df['Embarked'].fillna(train_df['Embarked'].mode()[0], inplace=True)
+test_df['Embarked'].fillna(test_df['Embarked'].mode()[0], inplace=True)
+
+# data preprocessing - dropping columns
+# 컬럼(열)을 삭제함
+train_df.drop(columns=['Cabin'], inplace=True)
+test_df.drop(columns=['Cabin'], inplace=True)
+
+# data preprocessing - Fare
+train_df['Fare'].fillna(train_df['Fare'].mean(), inplace=True)
+test_df['Fare'].fillna(test_df['Fare'].mean(), inplace=True)
